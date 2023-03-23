0ecae3dc5077fe45edfc912109e8bd4593eaa0f7
abu dzar al ghifari <abudzar.alghifari@ikonsultan.com>
2023-03-23 09:41:51 +0700

    fixing approved approval

--- src/store/reducer-approval.js
+++ src/store/reducer-approval.js
@@ -4,14 +4,18 @@ export const reducerApproval = createSlice({
   name: 'approval',
   initialState: {
     listModule:[],
+    uuidAproval:null,
   },
   reducers: {
     setListModule: (state, {payload} ) => {
       state.listModule = payload;
     },
+    setUuidApproval: (state, {payload} ) => {
+      state.uuidAproval = payload;
+    },
   }
 });
 
-export const {setListModule } = reducerApproval.actions;
+export const {setListModule,setUuidApproval } = reducerApproval.actions;
 
 export default reducerApproval.reducer;
