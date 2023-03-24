3a47f0733094df5742cd20c03d707c5a90a77214
abu dzar al ghifari <abudzar.alghifari@ikonsultan.com>
2023-03-24 18:47:52 +0700

    fixing styling dashboard

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,14 @@
 /** Libs */
 import * as React from 'react';
-import {Breadcrumbs,Link, Grid} from '@mui/material'
+import {Breadcrumbs,Link, Grid, Divider, Typography} from '@mui/material'
 import HomeIcon from '@mui/icons-material/Home';
 import { useSelector } from 'react-redux';
-// function handleClick(event) {
-//   event.preventDefault();
-//   console.info('You clicked a breadcrumb.');
-// }
+import ChartStatus from './component/chartStatus';
+import ChartUser from './component/chartUser';
+import ChartDepartment from './component/chartDepartment';
+import ChartRequest from './component/chartRequest';
+
+import {data} from './contants'
 /** Global Components */
 
 /** Local Components */
@@ -43,11 +45,57 @@ export default function Home() {
                     underline="hover"
                     sx={{ display: 'flex', alignItems: 'center' }}
                     color="inherit"
-                    href="/material-ui/getting-started/installation/"
+                    href="/"
                 >
                     Dashboard
                 </Link>
             </Breadcrumbs>
+
+            {/* header content */}
+            <Typography className='title-page' variant="h4" component="h4" mt={2}> 
+                Dashboard
+            </Typography>
+            <Grid 
+                container  
+                direction="row"
+                mt={4}
+            >
+                <Grid item xs={6}>
+                    <Typography className='info-subtitle-page' variant="h7" component="h7" > 
+                        Reporting dari seluruh aktivitas Procurement
+                    </Typography>
+                </Grid>
+                <Grid 
+                    container  
+                    direction="row"
+                    item xs={6}
+                    justifyContent="end"
+                    gap={2}
+                    >
+                    <Grid>
+                        {/* <CustomizeButton
+                            title="Reject"
+                            btn="button-reject"
+                            onClick={()=>setOpenModal(true)}
+                        /> */}
+                    </Grid>
+                </Grid>
+                <Grid
+                    container  
+                    direction="row"
+                    mt={4}
+                >
+                    <Grid item xs={6}><ChartStatus/></Grid>
+                    <Grid item xs={6}><ChartRequest data={data} xKey="name" yKey="pv"/></Grid>
+                    <Grid item xs={6}><ChartUser/></Grid>
+                    <Grid item xs={6}><ChartDepartment/></Grid>
+
+
+                </Grid>
+                
+            </Grid>
+            
+            <Divider style={{color:"#EAECF0", marginTop:"20px", marginBottom:"20px"}}/>
         </Grid>
         <Grid item xs={show ? 1 : 1}></Grid>
     </Grid>
